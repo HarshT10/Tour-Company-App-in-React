70e3802ccb03ad9c8ff8983e2c2bb52fbe14a99c
HarshT10 <harsh.thakkar1011@gmail.com>
2023-05-20 13:45:17 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,113 @@
+import img1 from "./assets/tour-1.jpeg";
+import img2 from "./assets/tour-2.jpeg";
+import img3 from "./assets/tour-3.jpeg";
+import img4 from "./assets/tour-4.jpeg";
+
+// Navbar Component Data
+export const pageLinks = [
+  {
+    id: 1,
+    href: "#home",
+    text: "home",
+  },
+  {
+    id: 2,
+    href: "#about",
+    text: "about",
+  },
+  {
+    id: 3,
+    href: "#services",
+    text: "services",
+  },
+  {
+    id: 4,
+    href: "#tours",
+    text: "tours",
+  },
+];
+
+// Navbar Component Data
+export const socialLinks = [
+  {
+    id: 1,
+    href: "https://facebook.com",
+    icon: "fab fa-facebook",
+  },
+  {
+    id: 2,
+    href: "https://twitter.com",
+    icon: "fab fa-twitter",
+  },
+  {
+    id: 3,
+    href: "https://squarespace.com",
+    icon: "fab fa-squarespace",
+  },
+];
+
+// Services Component Data
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+// Tours Component Data
+export const tours = [
+  {
+    id: 1,
+    img: img1,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    country: "china",
+    days: "6 days",
+    amount: "2100",
+  },
+  {
+    id: 2,
+    img: img2,
+    date: "october 1th, 2020",
+    title: "best of java",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    country: "indonesia",
+    days: "11 days",
+    amount: "1400",
+  },
+  {
+    id: 3,
+    img: img3,
+    date: "september 15th, 2020",
+    title: "explore hong kong",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    country: "hong kong",
+    days: "8 days",
+    amount: "5000",
+  },
+  {
+    id: 4,
+    img: img4,
+    date: "december 5th, 2019",
+    title: "kenya highlights",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    country: "kenya",
+    days: "20 days",
+    amount: "3300",
+  },
+];
